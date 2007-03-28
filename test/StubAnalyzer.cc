8b8efd770382b429648acb7a58e28492e6a05d42
Jason Daniel Keller <sha1-283d312d9b4f3ab9d562a4b7efb5ace0be730278@cern.ch>
2007-03-28 21:26:03 +0000

    Made changes to work with CMSSW_1_4_0_pre1

--- test/StubAnalyzer.cc
+++ test/StubAnalyzer.cc
@@ -13,7 +13,8 @@
 #include "RecoTracker/PixelStubs/test/StubAnalyzer.h"
 
 #include "DataFormats/TrackerRecHit2D/interface/SiPixelRecHitCollection.h"
-#include "FWCore/Framework/interface/Handle.h"
+//#include "FWCore/Framework/interface/Handle.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "DataFormats/TrackReco/interface/Track.h"
@@ -23,7 +24,8 @@
 #include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "Geometry/CommonDetUnit/interface/GeomDetType.h"
 #include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
-#include "Geometry/Vector/interface/GlobalPoint.h" 
+//#include "Geometry/Vector/interface/GlobalPoint.h" 
+#include "DataFormats/GeometryVector/interface/GlobalPoint.h"
 
 // Added this to debug -Tony
 #include "DataFormats/TrajectorySeed/src/classes.h"

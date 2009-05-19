d518e0210271d2734c9ac270fb64d9766c892f31
Dave Fehling Jr <sha1-8cf984741cfec7d737fdfbbe59c11fe82def7702@cern.ch>
2009-05-19 11:36:49 +0000

    Updated to 31x.

--- test/SealModules.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "RecoTracker/PixelStubs/test/StubAnalyzer.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(StubAnalyzer);
